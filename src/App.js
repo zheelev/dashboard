12308a1449aceb6146c4166f5d6be03d1b76ad10
zheelev <zheelev@gmail.com>
2025-05-06 00:37:07 +0300

    Implementation of components: Calendar, Form and Invoice.

--- src/App.js
+++ src/App.js
@@ -6,15 +6,14 @@ import Sidebar from "./scenes/global/SideBar"
 import Dashboard from "./scenes/dashboard"
 import Team from "./scenes/team"
 import Contacts from "./scenes/contacts"
-/* import Invoices  from "./scenes/invoices"
-
-import Bar from "./scenes/bar"
+import Invoices from "./scenes/invoices"
 import Form from "./scenes/form"
+import Calendar from "./scenes/calendar"
+/*import Bar from "./scenes/bar"
 import Line from "./scenes/line"
 import Pie from "./scenes/pie"
 import FAQ from "./scenes/faq"
 import Geography from "./scenes/geography"
-import Calendar from ".scene/alendar"
 import Calendar from ".scene/alendar" */
 
 
@@ -33,11 +32,11 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts/>} />
-              {/*  <Route path="/invoices" element={<Invoices/>} />
-           
-            <Route path="/form" element={<Form/>} />
-            <Route path="/bar" element={<Bar/>} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
+              {/* <Route path="/bar" element={<Bar/>} />
             <Route path="/pie" element={<Pie/>} />
             <Route path="/line" element={<Line/>} />
             <Route path="/faq" element={<FAQ/>} />
